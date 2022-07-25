93c798514e0359bc0fc49dc1908c1a5e1e8bb179
Сергей Игдиров <isaRoot@users.noreply.github.com>
2022-07-25 15:31:27 +0300

    Create config.py
    Config file has access information, and its mean that it must be only in local spaces

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '0000000000:ABCDEFGHabcdefgh'  # полученный у @BotFather
+
+pg_db_user = 'postgre_username'
+pg_db_password = 'postgre_password'
+pg_db_host = 'postgre_host'
+pg_db_port = '5432'
+pg_db_database = 'postgre_db_name'
+pg_db_schema = 'postgre_schema_name'
+
+bot_owner_id = 000000000
