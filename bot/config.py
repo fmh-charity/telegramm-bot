98c96aee8421da6e1d73e41e7c3ebf1e8ec277db
Sergei Igdirov aka isaroot <fsb.off@mail.ru>
2022-09-08 18:26:04 +0300

    Переделан бот для запуска с помощью докера

---
+++ bot/config.py
@@ -0,0 +1,11 @@
+import os
+
+TOKEN = os.environ['TOKEN']  # test_bot полученный у @BotFather
+
+pg_db_user = os.environ['pg_db_user']
+pg_db_password = os.environ['pg_db_password']
+pg_db_host = os.environ['pg_db_host']
+pg_db_port = os.environ['pg_db_port']
+pg_db_database = os.environ['pg_db_database']
+pg_db_schema = os.environ['pg_db_schema']
+bot_owner_id = os.environ['bot_owner_id']
